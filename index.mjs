1430180cd4cb35f09ba1fc9f1cb8b5b528619c4a
Son Nguyen <ngluunhatson@gmail.com>
2025-07-01 17:02:50 +0700

    First commit

---
+++ index.mjs
@@ -0,0 +1,31 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import "./loadEnv.mjs";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+// Basic route for the home page
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express API!");
+});
+
+// Error Handler Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the error for debugging
+  res.status(err.statusCode || 500).json({
+    status: "error",
+    message: err.message || "Internal Server Error",
+  });
+});
+
+mongoose.connect(process.env.MONGODB_DB_URI).then(() => {
+  console.log("Connected to Database");
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+});
